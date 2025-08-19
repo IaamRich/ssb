0e2ae9bdeafa1955de793e6602ed310b23b9762f
Vladimir Curaciov <vladimircuraciov@gmail.com>
2025-08-20 01:21:39 +0300

    added initial solution

---
+++ ssb.mobile/ssb.mobile/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ssb.mobile")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ssb.mobile.Pages")]
